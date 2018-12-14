9ea473e7fb335f436bdb7cb3cd5648f77edfa6fd
Edixon <edixon.tostring@gmail.com>
2018-12-13 23:35:48 -0400

    prueba 2

--- backend/index.php
+++ backend/index.php
@@ -1,13 +1,15 @@
 <?php
     include 'user.php';
 
-    $comando = $_GET['user'];
-    $usuarios = new userQuerys();
+    // $comando = $_GET['user'];
+    // $usuarios = new userQuerys();
     
-    if  ($comando == 'getAlls') {
-        echo $usuarios->get();
-    }
-    else {
-        echo 'command not valid';
-    }
+    // if  ($comando == 'getAlls') {
+    //     echo $usuarios->get();
+    // }
+    // else {
+    //     echo 'command not valid';
+    // }
+    $usuarios = new userQuerys();
+    echo $usuarios->get();
 ?>
