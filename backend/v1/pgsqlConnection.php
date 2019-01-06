ddb8f483fdc2fbf057c164119eba3b70d294ef41
Edixon <edixon.tostring@gmail.com>
2019-01-06 02:45:16 -0400

    mod de var entorno

--- backend/v1/pgsqlConnection.php
+++ backend/v1/pgsqlConnection.php
@@ -4,11 +4,7 @@ require 'dataBase.php';
 class pgsqlConnection {
 
     function connection() {
-<<<<<<< HEAD
         $dataPostgre = new dataBase('pgsql', 'DATABASE_URL');
-=======
-        $dataPostgre = new dataBase('pgsql', 'DATABASE_URL_LOCAL');
->>>>>>> 2d06b81720a8f4dda55105f12ce04cd7957ab225
 
         return $dataPostgre->connect();
     }
