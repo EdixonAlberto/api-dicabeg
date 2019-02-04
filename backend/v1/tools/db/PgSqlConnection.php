3effcd9c926702cd5d8ca6035d99c3e4b15688e8
Edixon <edixon.tostring@gmail.com>
2019-02-04 04:42:14 -0400

    var_env cambiando para deploy

--- backend/v1/tools/db/PgSqlConnection.php
+++ backend/v1/tools/db/PgSqlConnection.php
@@ -2,11 +2,11 @@
 
 require_once 'DataBase.php';
 
-class PgSqlConnection {
-
-    public static function connection() {
-        $dataPostgre = new dataBase('pgsql', 'DATABASE_URL_LOCAL');
-
+class PgSqlConnection
+{
+    public static function connection()
+    {
+        $dataPostgre = new dataBase('pgsql', 'DATABASE_URL');
         return $dataPostgre->connect();
     }
 }
