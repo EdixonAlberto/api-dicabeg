c838c3dd7eac24cc89e642f79f45c35ca263c6c0
Edixon <edixon.tostring@gmail.com>
2019-02-14 01:49:49 -0400

    se agrega codigo para definir zona horaria por defecto

--- backend/v1/tools/db/PgSqlConnection.php
+++ backend/v1/tools/db/PgSqlConnection.php
@@ -7,6 +7,7 @@ class PgSqlConnection
     public static function connection()
     {
         try {
+            date_default_timezone_set('America/Caracas');
             $dataPostgre = new DataBase('pgsql', 'DATABASE_URL');
             return $dataPostgre->connect();
         } catch (Exception $th) {
