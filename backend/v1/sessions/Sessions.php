fcea9179348fb5eafa552e8a1b9be7e44cd62d87
Edixon <edixon.tostring@gmail.com>
2019-02-22 23:09:32 -0400

    verificacion en el seteo del token

--- backend/v1/sessions/Sessions.php
+++ backend/v1/sessions/Sessions.php
@@ -66,21 +66,22 @@ class Sessions
 
     public static function verifySession()
     {
-        $session = SessionsQuerys::selectByToken();
-        if ($session) {
-            $sessionTime = strtotime($session->create_date . Options::expirationTime());
-            // $dev_sessionTime = date('Y-m-d H:i', $sessionTime);
-            // var_dump($dev_sessionTime);
+        if (isset($_SERVER['HTTP_API_TOKEN'])) {
+            $session = SessionsQuerys::selectByToken();
+            if ($session) {
+                $sessionTime = strtotime($session->create_date . Options::expirationTime());
+                // $dev_sessionTime = date('Y-m-d H:i', $sessionTime);
+                // var_dump($dev_sessionTime);
 
-            date_default_timezone_set('America/Caracas');
-            $expirationTime = strtotime(date('Y-m-d H:i'));
-            // $dev_expirationTime = date('Y-m-d H:i', $expirationTime);
-            // var_dump($dev_expirationTime);
+                date_default_timezone_set('America/Caracas');
+                $expirationTime = strtotime(date('Y-m-d H:i'));
+                // $dev_expirationTime = date('Y-m-d H:i', $expirationTime);
+                // var_dump($dev_expirationTime);
 
-            if ($expirationTime < $sessionTime);
-            else throw new Exception('token expired', 401);
-
-        } else throw new Exception('token incorrect', 401);
+                if ($expirationTime < $sessionTime);
+                else throw new Exception('token expired', 401);
+            } else throw new Exception('token incorrect', 401);
+        } else throw new Exception('not found token', 404);
     }
 
     public static function removeSession()
