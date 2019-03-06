84e885d4a352e8fa1ab22c2597038c961045152a
Edixon <edixon.tostring@gmail.com>
2019-03-06 16:52:14 -0400

    cambio en el uso de clases | se usa el verbo patch

--- backend/v1/options/index.php
+++ backend/v1/options/index.php
@@ -3,24 +3,14 @@
 require_once __DIR__ . '../../../../vendor/autoload.php';
 
 use Tools\JsonResponse;
-use V1\Options\Options;
+use V1\Options\Time;
 
 $method = $_SERVER['REQUEST_METHOD'];
 parse_str(file_get_contents('php://input'), $_REQUEST);
 
 if (isset($_GET['id'])) {
-   if ($_GET['id'] === 'time') {
-      if ($method == 'GET') {
-         $time = Options::expirationTime();
-         $time = trim($time, '+');
+   if ($_GET['id'] != 'time') JsonResponse::error('not found resource', 400);
 
-         echo json_encode(['expirationTime' => $time]);
-
-      } elseif ($method == 'POST') {
-         $time = Options::expirationTime();
-         $time = trim($time, '+');
-
-         Options::setExpirationTime($time, $_REQUEST['time'] . ' minute');
-      }
-   } else JsonResponse::error('not found resource', 400);
+   if ($method == 'GET') Time::getExpiration();
+   elseif ($method == 'PATCH') Time::setExpiration();
 }
