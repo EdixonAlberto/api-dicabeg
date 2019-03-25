2b9f1624e60865df0b1b58602c428074ae30ee32
Edixon <edixon.tostring@gmail.com>
2019-03-25 01:04:37 -0400

    mejorando code del nuevo query | utilizando bindValue para sanear los datos

--- api/v2/database/Querys.php
+++ api/v2/database/Querys.php
@@ -2,76 +2,78 @@
 
 namespace V2\Database;
 
-use V2\Database\QuerysExecute;
+use V2\Database\Execute;
 
-class Querys extends QuerysExecute
+class Querys extends Execute
 {
-   public function __construct()
-   {
-   }
+    public function __construct()
+    {
+    }
 
-   public static function table(string $table)
-   {
-      $query = new Querys();
-      $query->table = $table;
-      return $query;
-   }
+    public static function table(string $table)
+    {
+        $query = new Querys();
+        $query->table = $table;
+        return $query;
+    }
 
-   public function select(string $fields)
-   {
-      $this->sql = "SELECT {$fields} FROM {$this->table}";
-      return $this;
-   }
+    public function select(string $fields)
+    {
+        $this->sql = "SELECT {$fields} FROM {$this->table}";
+        return $this;
+    }
 
-   public function insert(array $arraySet)
-   {
-      $setInsert = $setValues = '';
-      $setLenght = count($arraySet);
-      $index = 1;
+    public function insert(array $arraySet)
+    {
+        $setInsert = $setValues = '';
+        $setLenght = count($arraySet);
+        $index = 1;
 
-      foreach ($arraySet as $set => $value) {
-         if ($index++ == $setLenght) {
-            $setInsert .= $set;
-            $setValues .= "'{$value}'";
-         } else {
-            $setInsert .= "{$set}, ";
-            $setValues .= "'{$value}', ";
-         }
-      }
+        foreach ($arraySet as $set => $value) {
+            if ($index++ == $setLenght) {
+                $setInsert .= $set;
+                $setValues .= '?';
+            } else {
+                $setInsert .= "{$set}, ";
+                $setValues .= '?, ';
+            }
+        }
 
-      $this->sql = "INSERT INTO {$this->table} ({$setInsert})
-                     VALUES ({$setValues})";
-      return $this;
-   }
+        $this->sql = "INSERT INTO {$this->table} ({$setInsert})
+                        VALUES ({$setValues})";
+        $this->arraySet = $arraySet;
+        return $this;
+    }
 
-   public function update(array $arraySet)
-   {
-      $setUpdate = '';
-      $setLenght = count($arraySet);
-      $index = 1;
+    public function update(array $arraySet)
+    {
+        $setUpdate = '';
+        $setLenght = count($arraySet);
+        $index = 1;
 
-      foreach ($arraySet as $set => $value) {
-         if ($index++ == $setLenght) {
-            $setUpdate .= "{$set} = '{$value}'";
-         } else {
-            $setUpdate .= "{$set} = '{$value}', ";
-         }
-      }
+        foreach ($arraySet as $set => $value) {
+            if ($index++ == $setLenght) {
+                $setUpdate .= "{$set} = ?";
+            } else {
+                $setUpdate .= "{$set} = ?, ";
+            }
+        }
 
-      $this->sql = "UPDATE {$this->table} SET {$setUpdate}";
-      return $this;
-   }
+        $this->sql = "UPDATE {$this->table} SET {$setUpdate}";
+        $this->arraySet = $arraySet;
+        return $this;
+    }
 
-   public function delete()
-   {
-      $this->sql = "DELETE FROM {$this->table}";
-      return $this;
-   }
+    public function delete()
+    {
+        $this->sql = "DELETE FROM {$this->table}";
+        return $this;
+    }
 
-   public function where(string $column, string $value)
-   {
-      $this->sql .= " WHERE {$column} = ?";
-      $this->value = $value;
-      return $this;
-   }
+    public function where(string $column, string $value)
+    {
+        $this->sql .= " WHERE {$column} = ?";
+        $this->value = $value;
+        return $this;
+    }
 }
