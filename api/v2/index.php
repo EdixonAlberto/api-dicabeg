0c5c31eb2cdd2dba0643171bafc4e2e48eb7e610
Edixon <edixon.tostring@gmail.com>
2019-05-04 16:19:48 -0400

    probando carga de libraries

--- api/v2/index.php
+++ api/v2/index.php
@@ -6,10 +6,10 @@ use V2\Modules\Requests;
 use V2\Libraries\PhpDotEnv;
 use V2\Modules\JsonResponse;
 
+$request = new Requests;
 new PhpDotEnv;
 
 try {
-    $request = new Requests;
 
     require 'routes/' . RESOURCE . 'Route.php';
     throw new Exception('route incorrect', 400);
