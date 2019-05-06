a60662986fd200e0ed1be1c7fdde1cac30e46e56
Edixon <edixon.tostring@gmail.com>
2019-05-06 03:51:22 -0400

    se agrega el tipo de dato de salida en el metodo sendEmail | se regresa el codigo http que devuelve la libreria SendGrid

--- api/v2/modules/Diffusion.php
+++ api/v2/modules/Diffusion.php
@@ -7,14 +7,18 @@ use V2\Modules\EmailTemplate;
 
 class Diffusion
 {
-    public static function sendEmail(string $email, EmailTemplate $template)
-    {
-        return SendGrid::generateEmail(
+    public static function sendEmail(
+        string $email,
+        EmailTemplate $template
+    ) : int {
+        $arrayStatus = SendGrid::generateEmail(
             $template::APP_EMAIL,
             $template->subject,
             $email,
             $template->html
         )->send();
+
+        return $arrayStatus['statusCode'];
     }
 
     public function sendNotification()
