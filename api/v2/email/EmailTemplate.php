08e60d3599c0b1fd4d5485664b8a233cee1b5402
EdixonAlberto <edixonalbertto@gmail.com>
2019-06-02 04:34:46 -0400

    cambiando nombre de la app en los email | removiendo uso de class Languages

--- api/v2/email/EmailTemplate.php
+++ api/v2/email/EmailTemplate.php
@@ -2,9 +2,9 @@
 
 namespace V2\Email;
 
-class EmailTemplate extends Languages
+class EmailTemplate
 {
-    public const APP_EMAIL = 'support@dicapp.com';
+    public const SUPPORT_EMAIL = 'dicabeg2019@gmail.com';
     private const PRIVACY_POLICY_LINK = '*';
     private static $code;
 
@@ -12,18 +12,17 @@ class EmailTemplate extends Languages
         string $_code,
         string $_language
     ) {
-        global $templateType, $language;
+        global $language;
 
-        $templateType = 'account_activation';
-        $language = $_language;
         self::$code = $_code;
+        $language = $_language;
 
         $html = self::generateEmail(
             '../v2/email/templates/accountActivationEmail.min.html'
         );
 
         $template = new EmailTemplate;
-        $template->subject = '¡Bienvenid@ a Dicapp! - Activa tu Cuenta';
+        $template->subject = '¡Bienvenid@ a Dicabeg! - Activa tu Cuenta';
         $template->html = $html;
         return $template;
     }
@@ -32,18 +31,17 @@ class EmailTemplate extends Languages
         string $_code,
         string $_language
     ) {
-        global $templateType, $_language;
+        global $_language;
 
-        $templateType = 'password_recovery';
-        $language = $_language;
         self::$code = $_code;
+        $language = $_language;
 
         $html = self::generateEmail(
             '../v2/email/templates/recoveryPasswordEmail.min.html'
         );
 
         $template = new EmailTemplate;
-        $template->subject = 'Recuperacion de Cuenta';
+        $template->subject = 'Recuperación de Cuenta';
         $template->html = $html;
         return $template;
     }
@@ -53,13 +51,13 @@ class EmailTemplate extends Languages
         $_file = fopen($file, 'r');
         $html = trim(fgets($_file), "'");
 
-        $html = preg_replace('|text1|', self::text(1), $html);
-        $html = preg_replace('|text2|', self::text(2), $html);
-        $html = preg_replace('|text3|', self::text(3), $html);
-        $html = preg_replace('|text4|', self::text(4), $html);
+        // $html = preg_replace('|text1|', self::text(1), $html);
+        // $html = preg_replace('|text2|', self::text(2), $html);
+        // $html = preg_replace('|text3|', self::text(3), $html);
+        // $html = preg_replace('|text4|', self::text(4), $html);
 
         $html = preg_replace('|CODE|', self::$code, $html);
-        $html = preg_replace('|APP_EMAIL|', self::APP_EMAIL, $html);
+        $html = preg_replace('|SUPPORT_EMAIL|', self::SUPPORT_EMAIL, $html);
         $html = preg_replace(
             '|PRIVACY_POLICY_LINK|',
             self::PRIVACY_POLICY_LINK,
