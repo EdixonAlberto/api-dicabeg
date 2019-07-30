4db4cfe7bba68df7d0f8dc611f0f619da3d0e3d9
EdixonAlberto <edixonalbertto@gmail.com>
2019-07-29 20:18:28 -0400

    cambiando path de img en template report

--- api/v2/views/templates/report.blade.php
+++ api/v2/views/templates/report.blade.php
@@ -18,7 +18,7 @@
         <tr>
             <td id="td-logo">
                 <img id="logo-dicabeg"
-                    src="https://{{ $_SERVER['SERVER_NAME'] }}/v2/public/img/dicabeg.png"
+                    src="https://{{ $_SERVER['SERVER_NAME'] }}/img/dicabeg.png"
                     alt="Logo Dicabeg" />
             </td>
             <td>
@@ -33,7 +33,7 @@
                 <p class="p-bold">{{ V2\Modules\EmailTemplate::$subject }}</p>
             </td>
             <td>
-                <img id="title-image" src="https://{{ $_SERVER['SERVER_NAME'] }}/v2/public/img/register.png" alt="Title Image" />
+                <img id="title-image" src="https://{{ $_SERVER['SERVER_NAME'] }}/img/register.png" alt="Title Image" />
             </td>
         </tr>
         <tr>
