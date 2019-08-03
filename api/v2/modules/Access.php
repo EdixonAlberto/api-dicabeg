0a3f85e1389927f77c04b585cdfa660c54ea042c
EdixonAlberto <edixonalbertto@gmail.com>
2019-08-02 23:40:50 -0400

    agregando nuevo dato en Access
    - enviando el tiempo que dura la sesion como respuesta en los
      datos de acceso

--- api/v2/modules/Access.php
+++ api/v2/modules/Access.php
@@ -4,6 +4,7 @@ namespace V2\Modules;
 
 use V2\Modules\Time;
 use V2\Libraries\Jwt;
+use V2\Database\Querys;
 
 class Access
 {
@@ -20,7 +21,20 @@ class Access
             'refresh_token' => $refresh->token,
             'expiration_date_unix' => $access->expiration_date,
             'expiration_date_utc' => date('Y-m-d H:i:s', $access->expiration_date),
-            'time_zone' => Time::$timeZone
+            'time_zone' => Time::$timeZone,
+            'expiration_time' => $this->expiration()
         ];
+    }
+
+    private function expiration(): string
+    {
+        // TODO: abstraer esto a class Time
+        $time = Querys::table('options')
+            ->select('expiration_time')
+            ->get();
+
+        $expiration_time = preg_replace('/( min)$/', '', $time);
+
+        return $expiration_time;
     }
 }
