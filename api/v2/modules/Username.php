82ef2ae646dcbe37d5bcc9dc96a437edff52acd1
EdixonAlberto <edixonalbertto@gmail.com>
2019-08-12 13:18:08 -0400

    resolviendo error en Username
    - error por usar un metodo deprecado en la clase Security
    - eliminado metodos de depuracion

--- api/v2/modules/Username.php
+++ api/v2/modules/Username.php
@@ -2,8 +2,8 @@
 
 namespace V2\Modules;
 
+use Modules\Tools\Code;
 use V2\Database\Querys;
-use V2\Modules\Security;
 use V2\Modules\JsonResponse;
 
 class Username
@@ -16,12 +16,11 @@ class Username
             ->get();
 
         if ($usernameRepeated != false) {
-            var_dump('entre');
-
             $newName = substr($name, 0, (strpos($name, '-') > 0) ?
                 strpos($name, '-') : strlen($name));
-            $newName .= '-' . Security::generateCode(4);
-            var_dump($newName);
+
+            $newName .= '-' . Code::create();
+
             self::validate($newName, true);
         } elseif ($repeted) JsonResponse::error([
             'message' => 'username exist',
